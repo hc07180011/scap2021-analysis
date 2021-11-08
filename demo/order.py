51060df3784373c3e0d615cb5b82fe89998c5bbc
henrychao <>
2021-11-08 22:19:21 +0800

    交易API

--- demo/order.py
+++ demo/order.py
@@ -4,9 +4,7 @@ import hmac
 from requests import Request, Session
 
 
-ts = int(time.time() * 1000)
-
-req_args = {
+req_args = dict({
     "market": "BTC-PERP",
     "side": "buy",
     "price": 10000,
@@ -16,23 +14,26 @@ req_args = {
     "ioc": False,
     "postOnly": False,
     "clientId": None
-}
+})
 
 request = Request("POST", "https://ftx.com/api/orders", json=req_args)
 prepared = request.prepare()
 
+ts = int(time.time() * 1000)
 signature_payload = f"{ts}{prepared.method}{prepared.path_url}".encode()
 if prepared.body:
     signature_payload += prepared.body
 
-signature_payload = signature_payload
+API_KEY = "ZEM121gMc2G-TH1WHuAXYSMZX_yY3bJQ3RdJInSu" // You have to replace this
+API_SECRET = "PqpL-ysuDEI4QDNjA-aThqtf62L5qKBHJ4B4kkxe" // You have to replace this
+
 signature = hmac.new(
-    "nTOyFdN14ycUleT5aJXOe7miQww7NXZulcpCSMIe".encode(),
+    API_SECRET.encode(),
     signature_payload,
     "sha256"
 ).hexdigest()
 
-prepared.headers["FTX-KEY"] = "3eCAuRqOyTSEg2tvfIw-CT0rS-MOtPkc7ajQdAZn"
+prepared.headers["FTX-KEY"] = API_KEY
 prepared.headers["FTX-SIGN"] = signature
 prepared.headers["FTX-TS"] = str(ts)
 
